7acb8b8141b86be7152fd9b9751ed5ea4860ac2e
Florian PENDARIES <flori@DESKTOP-67LHMEK>
2017-04-07 10:58:12 +0200

    Amélioration Catcheur Avancés dans MatchManager (combat)

--- el_chapo/Catcheur.cs
+++ el_chapo/Catcheur.cs
@@ -8,6 +8,7 @@ namespace el_chapo
 {
     public enum CatcheurType { Agile, Brute }
     public enum CatcheurState { Opérationnel, Convalescent, Mort }
+    public enum CatcheurAction { Attaque, Defend, AttaqueSpecial }
 
     public class Catcheur
     {
@@ -17,7 +18,10 @@ namespace el_chapo
         public int Vie { get; set; }
         public int Defense { get; set; }
         public int Attaque { get; set; }
-        
+
+        public CatcheurAction action { get; set; }
+       
+
 
         public Catcheur(string pseudo, CatcheurType type, CatcheurState state)
         {
@@ -29,12 +33,12 @@ namespace el_chapo
             switch (type)
             {
                 case CatcheurType.Agile:
-                    Vie = 125;
+                    Vie = 2; //125
                     Attaque = 3;
                     Defense = 3;
                     break;
                 case CatcheurType.Brute:
-                    Vie = 100;
+                    Vie = 3; //100
                     Attaque = 5;
                     Defense = 1;
                     break;
@@ -47,6 +51,46 @@ namespace el_chapo
             string description = $"{index}. NOM : {Pseudo} | TYPE : {CatcheurType} | HP : {Vie} | Attaque : {Attaque} | Defense : {Defense}";
 
             return description;
+        }
+
+        public void ChooseAction()
+        {
+            // On crée un objet de type Random qui permet de générer des nombres aléatoire (dice = dés)
+            
+            int action = MatchManager.dice.Next(1, 4);
+
+            switch (action)
+            {
+                case 1:
+                    this.action = CatcheurAction.Attaque;
+                    
+                    break;
+                case 2:
+                    this.action = CatcheurAction.Defend;
+                    
+                    
+                    break;
+                case 3:
+                    this.action = CatcheurAction.AttaqueSpecial;
+                    
+                    
+                    break;
+            }  
+        }
+
+        public Boolean AttaqueCible(Catcheur cible, int? defense)
+        {
+            if ((cible.Vie + cible.Defense) - this.Attaque > 0)
+            {
+                cible.Vie = (cible.Vie + cible.Defense) - this.Attaque;
+                return true;
+            }
+            else
+            {
+                cible.Vie = 0;
+                cible.CatcheurState = CatcheurState.Mort;
+                return false;
+            }
         }
     }
 }
--- el_chapo/MatchManager.cs
+++ el_chapo/MatchManager.cs
@@ -10,15 +10,17 @@ namespace el_chapo
     {
         public List<Catcheur> Catcheurs { get; set; }
         public static MatchManager instance = new MatchManager();
+        public static Random dice = new Random();
+        private Boolean someoneIsDead = false;
 
         private int opATM;
-        private Catcheur[] concurants;
 
         public MatchManager()
         {
             //Init catcheurs
             Catcheurs = new List<Catcheur>();
 
+            // Special = 0.3, Attaque = 0.35, Defense = 0.35
             Catcheurs.Add(new Catcheur("L'ordonnateur des pompes funèbres", CatcheurType.Brute, CatcheurState.Opérationnel));
             Catcheurs.Add(new Catcheur("Jude Sunny", CatcheurType.Brute, CatcheurState.Opérationnel));
             Catcheurs.Add(new Catcheur("Triple Hache", CatcheurType.Agile, CatcheurState.Opérationnel));
@@ -31,8 +33,6 @@ namespace el_chapo
             Catcheurs.Add(new Catcheur("Chris Hart", CatcheurType.Brute, CatcheurState.Opérationnel));
             Catcheurs.Add(new Catcheur("Bret Benoit", CatcheurType.Agile, CatcheurState.Opérationnel));
 
-            //Cache
-            concurants = new Catcheur[2];
         }
 
         public void CreateNewMatch()
@@ -60,8 +60,6 @@ namespace el_chapo
 
         public void chooseCatcheurs()
         {
-            concurants = new Catcheur[2];
-
             int tempP1;
             int tempP2 = int.MaxValue;
 
@@ -85,8 +83,124 @@ namespace el_chapo
             while (tempP2 == tempP1);
 
             Console.WriteLine($"Le combat va se disputer entre {Catcheurs[tempP1].Pseudo} & {Catcheurs[tempP2].Pseudo}");
+            ManageFight(Catcheurs[tempP1], Catcheurs[tempP2]);
 
+        }
 
+        private void ManageFight(Catcheur c1, Catcheur c2)
+        {
+            someoneIsDead = false;
+
+            for (int iteration = 1; iteration <= 20; iteration++)
+            {
+                if (!someoneIsDead)
+                {
+                    Console.WriteLine($"\n\nITERATION {iteration} !");
+
+                    // On détermine les actions à faire (random)
+                    c1.ChooseAction();
+                    c2.ChooseAction();
+
+                    // On lance le dé pour voir qui commence, si randomInt = 1 c1 commence / si randomInt = 2 c2 commence
+                    int randomInt = dice.Next(1, 3);
+
+                    if (randomInt == 2)
+                    {
+                        Catcheur tempC1 = c1;
+                        c1 = c2;
+                        c2 = tempC1;
+                    }
+
+                    // On détermine le combo d'action et on le transforme en string, chaque cas est représenté par un duo de nombre compris entre 0 et 2 :
+                    // 0 = Attaque
+                    // 1 = Defense
+                    // 2 = AttaqueSpe
+                    // On a donc 00, 01, 02, 10, 11, 12, 20, 21, 22
+                    string trinaryAction = "" + ((int)c1.action) + "" + ((int)c2.action);
+                    Console.WriteLine(GetMatchUpScreen(c1, c2));
+                    IterationMatchUp(trinaryAction, c1, c2);
+
+                    if (c1.CatcheurState == CatcheurState.Mort || c2.CatcheurState == CatcheurState.Mort)
+                    {
+                        Console.WriteLine("Le Match est terminé l'un des participant est mort...");
+                        someoneIsDead = true;
+                    }
+                    Console.WriteLine($"Resultat : \n{c1.Pseudo} : {c1.Vie} HP\n{c2.Pseudo} : {c2.Vie}HP");
+
+                }
+                else
+                {
+                    break;
+                }
+
+                Console.WriteLine("Break successfull");
+            }
+
+        }
+
+        private StringBuilder GetMatchUpScreen(Catcheur c1, Catcheur c2)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"\n{c1.Pseudo} - {c1.Vie}HP - Action : {c1.action}");
+            sb.AppendLine($"\n{c2.Pseudo} - {c2.Vie}HP - Action : {c2.action}");
+            return sb;
+        }
+
+        private void IterationMatchUp(String trinary, Catcheur c1, Catcheur c2)
+        {
+            switch (trinary)
+            {
+                // Attaque - Attaque
+                case "00": // fait
+                    Console.WriteLine($"{c1.Pseudo} attaque {c2.Pseudo} à hauteur de {c1.Attaque}");
+                    if (c1.AttaqueCible(c2, null)) // Si c1 attaque c2 est que c2 ne meurt pas
+                    {
+                        Console.WriteLine($"{c2.Pseudo} attaque {c1.Pseudo} à hauteur de {c2.Attaque}");
+                        if (!c2.AttaqueCible(c1, null))
+                        {
+                            // Le c1 est mort
+                            Console.WriteLine($"{c1.Pseudo} est mort sur le coup... FIN DU MATCH !");
+                        }
+
+                    }
+                    else
+                    {
+                        // Le c2 est mort
+                        Console.WriteLine($"{c2.Pseudo} est mort sur le coup... FIN DU MATCH !");
+                    }
+                    Console.WriteLine($"Resultat : \n{c1.Pseudo} : {c1.Vie} HP\n{c2.Pseudo} : {c2.Vie}HP");
+                    break;
+
+                // Attaque - Defense
+                case "01": //fait
+                    Console.WriteLine($"{c1.Pseudo} attaque {c2.Pseudo} à hauteur de {c1.Attaque}, {c2.Pseudo} aborsbe {c2.Defense} point(s) de dégat !");
+                    break;
+
+                case "02":
+                    // On traite les attaques spéciales plus tard hein...
+                    break;
+                
+                // Defense - Attaque
+                case "10":
+                    Console.WriteLine($"{c2.Pseudo} attaque {c1.Pseudo} à hauteur de {c2.Attaque}, {c1.Pseudo} aborsbe {c1.Defense} point(s) de dégat !");
+                    break;
+                // Defense - Defense MDR
+                case "11":
+                    Console.WriteLine($"{c1.Pseudo} et {c2.Pseudo} se regardent droit dans les yeux, tous deux en position de défense, malheureusement personne ne décidera d'attaquer ce tour-ci...");
+                    break;
+                case "12":
+                    // On traite les attaques spéciales plus tard hein...
+                    break;
+                case "20":
+
+                    break;
+                case "21":
+
+                    break;
+                case "22":
+
+                    break;
+            }
         }
 
 
